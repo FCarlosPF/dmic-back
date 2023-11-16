ab8224f67ebe83cfa8505aff854061a39c9fea8a
Carlos <paucarflorescarlos@gmail.com>
2023-11-16 11:06:51 -0500

    img-blobcorrection4

--- src/catalogo/catalogo.service.ts
+++ src/catalogo/catalogo.service.ts
@@ -4,34 +4,53 @@ import { UpdateCatalogoDto } from './dto/update-catalogo.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Catalogo } from './entities/catalogo.entity';
 import { Repository } from 'typeorm';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class CatalogoService {
-
-  constructor(@InjectRepository(Catalogo) private readonly catalogoRepository : Repository<Catalogo>){}
+  constructor(
+    @InjectRepository(Catalogo)
+    private readonly catalogoRepository: Repository<Catalogo>,
+  ) {}
 
   async create(createCatalogoDto: CreateCatalogoDto) {
-    return this.catalogoRepository.save(createCatalogoDto)
+    return this.catalogoRepository.save(createCatalogoDto);
   }
 
   async findAll() {
-    return await this.catalogoRepository.find()
+    const catalogos =  await this.catalogoRepository.find();
+
+    const catalogosConImagenesDecodificadas = await Promise.all(
+      catalogos.map(async (catalogo) => {
+        catalogo.imagen = await this.decodeImage(catalogo.imagen);
+        return catalogo;
+      }),
+    );
+
+    return catalogosConImagenesDecodificadas;
+
   }
 
   async findOne(iqms: number) {
-    const resultados = await this.catalogoRepository.findBy({iqms});
+    const resultados = await this.catalogoRepository.findBy({ iqms });
     if (resultados.length > 0) {
       return resultados[0];
     }
     return null;
-
   }
 
   async update(iqms: number, updateCatalogoDto: UpdateCatalogoDto) {
-    return await this.catalogoRepository.update(iqms,updateCatalogoDto);
+    return await this.catalogoRepository.update(iqms, updateCatalogoDto);
   }
 
   async remove(iqms: number) {
     return await this.catalogoRepository.delete(iqms);
+  }
+
+  private async decodeImage(imagen: Buffer): Promise<Buffer> {
+    // Perform decoding operations if necessary
+    // For example, you can use sharp here
+    // Note: Make sure to adjust the logic according to your specific needs
+    return sharp(imagen).resize(200, 200).toBuffer();
   }
 }
--- src/catalogo/dto/create-catalogo.dto.ts
+++ src/catalogo/dto/create-catalogo.dto.ts
@@ -12,5 +12,5 @@ export class CreateCatalogoDto {
     molde: string
 
     @IsString()
-    foto: Buffer
+    imagen: Buffer
 }
